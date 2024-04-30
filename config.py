f2173fbf633ea2d84fc8400c023779f8930a6471
U.S <93845604+OtotaO@users.noreply.github.com>
2024-04-29 23:30:56 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+# Path to the knowledge base
+KNOWLEDGE_BASE_PATH = 'knowledge_base.json'
+
+# Path to the data sources
+DATA_SOURCES = ['data_source1.json', 'data_source2.json']
+
+# Number of topics for LDA
+NUM_TOPICS = 2
+
+# Path to the stop words file
+STOP_WORDS_FILE = 'stop_words.txt'
+
+# Path to the lemmatizer model
+LEMMATIZER_MODEL = 'lemmatizer_model.pkl'
+
+# Path to the vectorizer model
+VECTORIZER_MODEL = 'vectorizer_model.pkl'
+
+# Path to the Naive Bayes model
+NAIVE_BAYES_MODEL = 'naive_bayes_model.pkl'
+
+# Path to the saved progress
+PROGRESS_FILE = 'progress.json'
+
+# Path to the topic model visualization
+TOPIC_MODEL_VISUALIZATION = 'topic_model_visualization.png'
