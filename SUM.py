83df1c13773cd46184ef3c3675e967fdde5a78c0
Ototao <93845604+OtotaO@users.noreply.github.com>
2024-06-17 18:08:54 -0400

    Update SUM.py

--- SUM.py
+++ SUM.py
@@ -1,130 +1,135 @@
 import json
-import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-from gensim.models import Word2Vec
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import spacy
+from collections import Counter
 import networkx as nx
 import matplotlib.pyplot as plt
 
 class SUM:
-    def __init__(self):
-        self.data = []
-        self.stopwords = set(stopwords.words('english'))
-        self.word2vec_model = None
-        self.running_summary = ''
+def __init__(self):
+self.stop_words = set(stopwords.words('english'))
+self.lemmatizer = WordNetLemmatizer()
+self.vectorizer = TfidfVectorizer()
+self.nlp = spacy.load('en_core_web_lg')
+self.running_summary = ""
 
-    def load_data(self, data_source):
-        with open(data_source, 'r') as file:
-            self.data = json.load(file)
+def load_data(self, data_source):
+with open(data_source, 'r') as file:
+data = json.load(file)
+return data
 
-    def preprocess_text(self, text):
-        text = re.sub(r'[^a-zA-Z\s]', '', text.lower())
-        tokens = word_tokenize(text)
-        filtered_tokens = [token for token in tokens if token not in self.stopwords]
-        return ' '.join(filtered_tokens)
+def preprocess_text(self, text):
+words = word_tokenize(text.lower())
+words = [self.lemmatizer.lemmatize(word) for word in words if word not in self.stop_words]
+preprocessed_text = ' '.join(words)
+return preprocessed_text
 
-    def generate_summaries(self, texts, max_length=100, min_length=30):
-        summaries = []
-        for text in texts:
-            sentences = sent_tokenize(text)
-            sentence_vectors = [self.get_sentence_vector(sentence) for sentence in sentences]
-            sentence_scores = self.calculate_sentence_scores(sentence_vectors)
-            summary = self.generate_summary(sentences, sentence_scores, max_length, min_length)
-            summaries.append(summary)
-            self.update_running_summary(summary)
-        return summaries
+def generate_summaries(self, texts, max_length=100, min_length=30):
+summaries = []
+for text in texts:
+sentences = sent_tokenize(text)
+sentence_scores = self.calculate_sentence_scores(sentences)
+summary = self.get_top_sentences(sentences, sentence_scores, max_length, min_length)
+summaries.append(summary)
+self.update_running_summary(summary)
+return summaries
 
-    def get_sentence_vector(self, sentence):
-        tokens = word_tokenize(sentence)
-        vector = sum([self.word2vec_model.wv[token] for token in tokens if token in self.word2vec_model.wv]) / len(tokens)
-        return vector
+def update_running_summary(self, summary):
+self.running_summary += summary + " "
 
-    def calculate_sentence_scores(self, sentence_vectors):
-        sentence_scores = {}
-        for i, vector in enumerate(sentence_vectors):
-            score = sum([vector.dot(other_vector) for other_vector in sentence_vectors]) / (len(sentence_vectors) - 1)
-            sentence_scores[i] = score
-        return sentence_scores
+def identify_entities(self, text):
+doc = self.nlp(text)
+entities = [(ent.text, ent.label_) for ent in doc.ents]
+return entities
 
-    def generate_summary(self, sentences, sentence_scores, max_length, min_length):
-        summary_sentences = sorted([(score, i, sentence) for i, (score, sentence) in enumerate(zip(sentence_scores.values(), sentences))], reverse=True)
-        summary = ''
-        for score, i, sentence in summary_sentences:
-            if len(summary) + len(sentence) < max_length and len(summary) < min_length:
-                summary += ' ' + sentence
-        return summary
+def identify_main_concept(self, text):
+doc = self.nlp(text)
+noun_chunks = [chunk.text for chunk in doc.noun_chunks]
+main_concept = max(noun_chunks, key=noun_chunks.count)
+return main_concept
 
-    def update_running_summary(self, summary):
-        self.running_summary += ' ' + summary
+def identify_main_direction(self, text):
+doc = self.nlp(text)
+verbs = [token.lemma_ for token in doc if token.pos_ == 'VERB']
+main_direction = max(verbs, key=verbs.count)
+return main_direction
 
-    def identify_entities(self, text):
-        # Implement entity identification using NLP libraries like spaCy or NLTK
-        pass
+def calculate_similarity(self, text1, text2):
+doc1 = self.nlp(text1)
+doc2 = self.nlp(text2)
+similarity = doc1.similarity(doc2)
+return similarity
 
-    def identify_main_concept(self, text):
-        # Implement main concept identification using NLP techniques
-        pass
+def process_knowledge_base(self, knowledge_base):
+processed_kb = {}
+for concept, value in knowledge_base.items():
+processed_kb[concept] = self.preprocess_text(value)
+return processed_kb
 
-    def identify_main_direction(self, text):
-        # Implement main direction identification using NLP techniques
-        pass
+def identify_topics(self, summaries):
+topics = []
+for summary in summaries:
+doc = self.nlp(summary)
+topic = self.identify_main_concept(doc.text)
+topics.append(topic)
+return topics
 
-    def calculate_similarity(self, text1, text2):
-        # Implement text similarity calculation using techniques like cosine similarity
-        pass
+def build_knowledge_graph(self, topics):
+G = nx.Graph()
+for topic in topics:
+G.add_node(topic)
+for other_topic in topics:
+if topic != other_topic:
+similarity = self.calculate_similarity(topic, other_topic)
+if similarity > 0.5:
+G.add_edge(topic, other_topic, weight=similarity)
+return G
 
-    def process_knowledge_base(self):
-        # Implement knowledge base processing
-        pass
+def visualize_knowledge_graph(self, G):
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True, node_size=1000, font_size=12, edge_color='gray', node_color='skyblue')
+labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+plt.axis('off')
+plt.show()
 
-    def identify_topics(self, summaries):
-        # Implement topic identification using techniques like LDA or NMF
-        pass
+def calculate_sentence_scores(self, sentences):
+vectors = self.vectorizer.fit_transform(sentences)
+scores = cosine_similarity(vectors[-1], vectors[:-1])[0]
+return scores
 
-    def build_knowledge_graph(self, topics):
-        # Implement knowledge graph construction using libraries like NetworkX
-        pass
+def get_top_sentences(self, sentences, scores, max_length, min_length):
+ranked_sentences = sorted(zip(scores, sentences), reverse=True)
+summary = ranked_sentences[0][1]
+for i in range(1, len(ranked_sentences)):
+if len(summary) + len(ranked_sentences[i][1]) <= max_length:
+summary += " " + ranked_sentences[i][1]
+else:
+break
+if len(summary) < min_length:
+for i in range(len(ranked_sentences)):
+if len(summary) + len(ranked_sentences[i][1]) <= min_length:
+summary += " " + ranked_sentences[i][1]
+else:
+break
+return summary
 
-    def visualize_knowledge_graph(self):
-        # Implement knowledge graph visualization using libraries like NetworkX and Matplotlib
-        pass
+def run(self, data_source, knowledge_base_source):
+data = self.load_data(data_source)
+preprocessed_data = [self.preprocess_text(text) for text in data]
+summaries = self.generate_summaries(preprocessed_data)
 
-    def run(self):
-        # Load data
-        self.load_data('data.json')
+print("Final Running Summary:")
+print(self.running_summary)
 
-        # Preprocess text
-        preprocessed_data = [self.preprocess_text(text) for text in self.data]
-
-        # Train Word2Vec model
-        self.word2vec_model = Word2Vec(preprocessed_data, min_count=1)
-
-        # Generate summaries
-        summaries = self.generate_summaries(preprocessed_data)
-
-        # Identify entities, main concepts, and main directions
-        for summary in summaries:
-            entities = self.identify_entities(summary)
-            main_concept = self.identify_main_concept(summary)
-            main_direction = self.identify_main_direction(summary)
-
-        # Process knowledge base
-        self.process_knowledge_base()
-
-        # Identify topics
-        topics = self.identify_topics(summaries)
-
-        # Build knowledge graph
-        knowledge_graph = self.build_knowledge_graph(topics)
-
-        # Visualize knowledge graph
-        self.visualize_knowledge_graph(knowledge_graph)
-
-        # Print running summary
-        print("Running Summary:")
-        print(self.running_summary)
-
-if __name__ == '__main__':
-    sum_platform = SUM()
-    sum_platform.run()
+knowledge_base = self.load_data(knowledge_base_source)
+processed_kb = self.process_knowledge_base(knowledge_base)
+topics = self.identify_topics(summaries)
+G = self.build_knowledge_graph(topics)
+self.visualize_knowledge_graph(G)
+```
